e29a410562b43d8d3c3dc0a13f635675759ef276
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-05-15 14:09:24 +0500

    Time: 43 ms (18.23%) | Memory: 16.5 MB (42.70%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
@@ -1,15 +1,12 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        lower_set = set()
-        upper_set = set()
-    
-        for char in word:
-            if char.islower():
-                lower_set.add(char)
-            elif char.isupper():
-                upper_set.add(char)
-    
-        # Find the intersection of the two sets
-        special_letters = lower_set.intersection(char.lower() for char in upper_set)
+
+        word = set(word)
+        count = 0
+
+        for i in word:
+            if i.lower() == i:
+                if i.upper() in word:
+                    count += 1
         
-        return len(special_letters)
+        return count
