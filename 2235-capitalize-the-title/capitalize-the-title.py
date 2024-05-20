2f138dff7eab818ff25f5ee9525b676cb6f0fdaf
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-05-20 08:45:50 +0500

    Time: 41 ms (18.75%) | Memory: 16.6 MB (13.60%) - LeetSync

---
+++ 2235-capitalize-the-title/capitalize-the-title.py
@@ -0,0 +1,13 @@
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        string = ""
+
+        x = title.split()
+
+        for i in x:
+            if len(i) > 2:
+                string = string + i.title() + " "
+            else:
+                string = string + i.lower() + " "
+            
+        return string.rstrip()
