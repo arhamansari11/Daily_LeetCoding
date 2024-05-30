e0fbd606879e8ae957ded6fe0eff2b5b115a137b
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-05-30 11:25:20 +0500

    Time: 245 ms (86.75%) | Memory: 27.3 MB (35.95%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -1,16 +1,9 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        # Iterate through the nums array
-        for num in nums:
-            # Get the index corresponding to the absolute value of num - 1
-            index = abs(num) - 1
-            # If nums[index] is positive, mark it as visited by making it negative
-            if nums[index] > 0:
-                nums[index] *= -1
-        # Iterate through the modified nums array to find which indices were not marked as visited
-        result = []
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                # If nums[i] is positive, it means that the number corresponding to index i + 1 did not appear in nums
-                result.append(i + 1)
-        return result
+        set1 = set(nums)
+        ans = []
+        for i in range(1 , len(nums)+1):
+            if i not in set1:
+                ans.append(i)
+
+        return ans
