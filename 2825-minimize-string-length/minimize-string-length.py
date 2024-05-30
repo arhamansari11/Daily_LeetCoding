d30dd40b86f4099ef883b330c9e7dbdd76ab2f99
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-05-30 11:58:23 +0500

    Time: 52 ms (40.15%) | Memory: 16.4 MB (93.46%) - LeetSync

--- 2825-minimize-string-length/minimize-string-length.py
+++ 2825-minimize-string-length/minimize-string-length.py
@@ -1,8 +1,16 @@
 class Solution:
     def minimizedStringLength(self, s: str) -> int:
-        set1 = set(s)
+        # set1 = set(s)
+        # string = ""
+        # for i in set1:
+        #     string += i
+
+        # return len(string)
+
         string = ""
-        for i in set1:
-            string += i
+
+        for i in s:
+            if i not in string:
+                string += i
 
         return len(string)
