aecb4a279fe06de44d963919d3f6992fc691933b
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-05-31 18:00:43 +0500

    Time: 3080 ms (8.68%) | Memory: 18.6 MB (60.05%) - LeetSync

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -1,7 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         arr = []
-
         for i in nums:
             if nums.count(i) == 1:
                 arr.append(i)
