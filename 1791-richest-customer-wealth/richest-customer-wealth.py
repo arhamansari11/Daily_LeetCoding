23909b3b9f4a568194aa2a57907a922439024896
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-06-03 06:20:19 +0500

    Time: 51 ms (70.13%) | Memory: 16.4 MB (95.13%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.py
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -1,10 +1,10 @@
 class Solution:
     def maximumWealth(self, nums: List[List[int]]) -> int:
         result = []
-
         for i in range(len(nums)):
             add = sum(nums[i])
             result.append(add)
 
         maximum = max(result)
+
         return maximum
