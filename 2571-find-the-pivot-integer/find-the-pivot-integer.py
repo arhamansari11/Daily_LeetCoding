74e7a88492f13ea643ddf4a91eedd46cfa16c0a5
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-06-19 20:52:42 +0500

    Time: 56 ms (40.78%) | Memory: 16.4 MB (77.87%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        arr = []
+        i = 1
+        while i <= n:
+            arr.append(i)
+            i += 1
+        
+        total_sum = sum(arr)
+        leftsum = 0
+        for i in range(len(arr)):
+            if leftsum == (total_sum - leftsum - arr[i]):
+                return i + 1
+            
+            leftsum += arr[i]
+
+        return -1
