821e3e43388d5a7fec411cfa01d4b9256e270c77
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-06-24 05:50:30 +0500

    Time: 36 ms (91.23%) | Memory: 16.3 MB (96.98%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -3,28 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)  # size of the array.
-         #[2,1,5,4,3,0,0]
-        # Step 1: Find the break point:
-        ind = -1  # break point
-        for i in range(n - 2, -1, -1):
-            if nums[i] < nums[i + 1]:
-                # index i is the break point
+        n = len(nums)
+        ind = -1
+        for i in range(n - 2 , -1 , -1):
+            if nums[i] < nums[i+1]:
                 ind = i
                 break
-       #[5,4,3,2,1]
-        # If break point does not exist:
+            
         if ind == -1:
-            # reverse the whole array:
             nums.reverse()
             return
-
-        # Step 2: Find the next greater element
-        #         and swap it with nums[ind]:
-        for i in range(n - 1, ind, -1):
+        
+        for i in range(n-1 , -1 , -1):
             if nums[i] > nums[ind]:
-                nums[i], nums[ind] = nums[ind], nums[i]
+                nums[ind] , nums[i] = nums[i] , nums[ind]
                 break
 
-        # Step 3: reverse the right half:
-        nums[ind + 1:] = reversed(nums[ind + 1:])
+        nums[ind + 1 :] = reversed(nums[ind + 1 :])
+
+
