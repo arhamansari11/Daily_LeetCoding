6a127285a864e17f604e305e215a46e891daa113
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-06-26 19:37:11 +0500

    Time: 53 ms (73.84%) | Memory: 18 MB (54.94%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.py
+++ 402-remove-k-digits/remove-k-digits.py
@@ -3,13 +3,12 @@ class Solution:
         if len(nums) == k:
             return "0"
         stack = []
-
         for i in range(len(nums)):
             while stack and stack[-1] > nums[i] and k > 0:
                 stack.pop()
                 k -= 1
             stack.append(nums[i])
         
-        stack = stack[:len(stack) - k]
+        stack = stack[:len(stack)- k]
 
         return "".join(stack).lstrip("0") or "0"
