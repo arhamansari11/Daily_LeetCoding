c475b0c57941d5cb225af118101606fee11d74b1
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-07-18 06:31:48 +0500

    Time: 162 ms (86.27%) | Memory: 20.8 MB (45.55%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -6,7 +6,8 @@ class Solution:
         left = 0
         right = len(nums) - 1
         while left < right:
-            nums[left] , nums[right] = nums[right] , nums[left]
+            if nums[left] != nums[right]:
+                nums[left] , nums[right] = nums[right] , nums[left]
             left += 1
             right -= 1
             
