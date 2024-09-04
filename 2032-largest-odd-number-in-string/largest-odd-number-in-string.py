813e4f8f4cb30571101908fe87c696bd57ca016a
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-09-04 09:08:43 +0500

    Time: 58 ms (26.44%) | Memory: 17.2 MB (100.00%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -1,12 +1,8 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        if int(num[-1]) % 2 != 0:
-            return num
         
-        # Iterate from the end of the string to find the last odd digit
         for i in range(len(num) - 1, -1, -1):
             if int(num[i]) % 2 != 0:
                 return num[:i + 1]
         
-        # If no odd digit is found, return an empty string
         return ""
