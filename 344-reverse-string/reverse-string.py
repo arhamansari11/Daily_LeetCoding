04ab0f9e8c908d311a31a50f3be3a87ba3b5e0b3
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-10-28 20:21:53 +0500

    Time: 7 ms (10.24%) | Memory: 22.1 MB (6.92%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,12 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """ 
-        def Reverse(arr , l , r):
-            if l >= r:
-                return
-            else:
-                arr[l] , arr[r] = arr[r] , arr[l]
-                return Reverse(arr , l+1 , r-1)
+
         l = 0
         r = len(nums) - 1
-        return Reverse(nums , l , r )
+        while l <= r:
+            nums[l] , nums[r] = nums[r] , nums[l]
+            l += 1
+            r -= 1
+
+
+        
