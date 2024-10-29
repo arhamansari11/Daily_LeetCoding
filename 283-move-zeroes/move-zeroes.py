db4c1305db1f6425861bce19c8e8e712d1d6d156
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-10-29 21:53:36 +0500

    Time: 3 ms (82.94%) | Memory: 17.7 MB (89.77%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,10 +4,8 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         left = 0
-        for right in range(len(nums)):
-            if nums[right] != 0:
-                nums[right] , nums[left] = nums[left] , nums[right]
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[left] , nums[i] = nums[i] , nums[left]
                 left += 1
 
-
-        return nums
