fcf3de21918cdd268c0620b23cd2e7e7147be4d4
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2024-12-18 23:04:34 +0500

    Time: 3 ms (55.13%) | Memory: 18 MB (10.22%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,14 +1,13 @@
 class Solution:
-    def finalPrices(self, nums: List[int]) -> List[int]:
-        arr = []
-        for i in range(len(nums)):
-            found = False
-            for j in range(i+1 , len(nums)):
-                if nums[i] >= nums[j]:
-                    arr.append(nums[i] - nums[j])
-                    found = True
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        final_price = []
+        for i in range(len(prices)):
+            discount = prices[i]
+            for j in range(i + 1 , len(prices)):
+                if prices[j] <= prices[i]:
+                    discount = prices[i] - prices[j]
                     break
-            if not found:
-                arr.append(nums[i])
 
-        return arr
+            final_price.append(discount)
+
+        return final_price
