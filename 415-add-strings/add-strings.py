30724436d078bd79b2f9ff56c9134350dc567101
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2025-01-19 15:44:00 +0500

    Time: 3 ms (89.55%) | Memory: 18 MB (40.23%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -1,6 +1,6 @@
-import sys 
-sys.set_int_max_str_digits(0)
+import sys
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        integer = int(num1) + int(num2)
-        return str(integer)
+        sys.set_int_max_str_digits(50000)
+        s = int(num1) + int(num2)
+        return str(s)
