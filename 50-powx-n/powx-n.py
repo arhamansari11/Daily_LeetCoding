b2f9e25f6ff2468c87feab021732544f21cd442d
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2025-01-20 18:28:06 +0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (49.53%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,9 +1,3 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        def power( x , n ):
-            if n == 0:
-                return 1
-            
-            return x ** n
-
-        return power( x  , n)
+        return x ** n
