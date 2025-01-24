570ebfba95cef1c606fd7b9f50ba6be5ed0e0b97
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2025-01-24 19:04:39 +0500

    Time: 7 ms (81.28%) | Memory: 18.2 MB (46.81%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,15 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        def Palindrome(arr , l , r):
-            if l >= r:
-                return True
-            if arr[l] != arr[r]:
-                return False
-            return Palindrome(arr , l + 1 , r - 1)
         string = ""
         for i in s:
-            if i.isalnum():
+            if i.isalpha() or i.isdigit():
                 string += i.lower()
-        l = 0
-        r = len(string) - 1
-        return Palindrome(string , l , r)
+
+        if string == string[::-1]:
+            return True
+        else:
+            return False
