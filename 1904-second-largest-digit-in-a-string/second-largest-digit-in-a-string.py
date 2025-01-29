f31adb7c5b1ca4090842adf55dcb9764f083bd95
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2025-01-29 18:53:28 +0500

    Time: 5 ms (31.18%) | Memory: 17.6 MB (66.75%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.py
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.py
@@ -3,13 +3,12 @@ class Solution:
         arr = []
         for i in s:
             if i.isdigit():
-                arr.append(int(i))
+                if int(i) not in arr:
+                    arr.append(int(i))
         
-        set1 = set(arr)
-        list1 = list(set1)
-        list1.sort()
-        if len(list1) >= 2:
-            return list1[-2]
-        
-        return -1
+        arr.sort()
+        length = len(arr)
+        if length >= 2:
+            return arr[length-2]
 
+        return -1
