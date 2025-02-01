8d1dabb4b8fb94eacbcf3036d4db244fbe3b5fea
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2025-02-01 19:11:51 +0500

    Time: 3 ms (65.69%) | Memory: 19 MB (81.64%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,25 +1,31 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-
         for i in tokens:
-            if i == "+":
-                op1 = stack.pop()
-                op2 = stack.pop()
-                stack.append(op2 + op1)
-            elif i == "-":
-                op1 = stack.pop()
-                op2 = stack.pop()
-                stack.append(op2 - op1)
-            elif i == "*":
-                op1 = stack.pop()
-                op2 = stack.pop()
-                stack.append(op2 * op1)
-            elif i == "/":
-                op1 = stack.pop()
-                op2 = stack.pop()
-                stack.append(int(op2 / op1))
-            else:
-                stack.append(int(i))
-            
+            if len(stack) >= 2:
+                if i == "+":
+                    op1 = stack.pop()
+                    op2 = stack.pop()
+                    stack.append(op1 + op2)
+                    continue
+                elif i == "-":
+                    op1 = stack.pop()
+                    op2 = stack.pop()
+                    stack.append(op2 - op1)
+                    continue
+                elif i == "*":
+                    op1 = stack.pop()
+                    op2 = stack.pop()
+                    stack.append(op2 * op1)
+                    continue
+                elif i == "/":
+                    op1 = stack.pop()
+                    op2 = stack.pop()
+                    stack.append(int(op2 / op1))
+                    continue
+                
+            stack.append(int(i))
+
         return stack[0]
+
+                
