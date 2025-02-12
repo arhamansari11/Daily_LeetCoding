774f64071173c0e3b63ef70ea29c5f06e28905c4
Muhammad Arham <arhamansari454647@gmail.com>
2025-02-12 12:03:23 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (19.37%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+        divisors = 0
+        i = 1
+        while i <= n:
+            if n % i == 0:
+                divisors += 1
+
+            i += 1
+
+        if divisors == 3:
+            return True
+        else:
+            return False
