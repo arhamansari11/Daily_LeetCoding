14a66d64523a1ec62b8b76f84f0626f647321a2e
Muhammad Arham <arhamansari454647@gmail.com>
2025-02-12 18:49:59 +0500

    Time: 0 ms (100.00%) | Memory: 18.5 MB (25.23%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,10 +1,13 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+
+        # Binary Search is only applicable on sorting array.
+
         l = 0
         r = len(nums) - 1
 
         while l <= r:
-            mid = (l + r ) // 2
+            mid = (l+r) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
@@ -13,4 +16,3 @@ class Solution:
                 r = mid - 1
 
         return l
-                
