55cd80e5d0724e3f076c21d26bf59101aed2225c
Muhammad Arham <arhamansari454647@gmail.com>
2025-02-26 19:07:06 +0500

    Time: 54 ms (82.14%) | Memory: 58.6 MB (67.14%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
@@ -5,20 +5,14 @@
 #         self.next = next
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
-
-        # Use a set for nums to enable O(1) lookup times
-        nums_set = set(nums)  # No duplicates, so set is optimal
+        nums_set = set(nums)
         dummy = ListNode(0)
         dummy.next = head
         curr = dummy
-        
-        # Traverse and modify the linked list
         while curr.next:
-            if curr.next.val in nums_set:  # Efficient O(1) lookup
-                curr.next = curr.next.next  # Remove the node by skipping it
+            if curr.next.val in nums_set:
+                curr.next = curr.next.next
             else:
-                curr = curr.next  
-                
+                curr = curr.next
+
         return dummy.next
