e5d6e4b6e5753b8e05e6b4c84e7fbf1503cf7283
Muhammad Arham <arhamansari454647@gmail.com>
2025-03-06 13:03:26 +0500

    Time: 347 ms (11.57%) | Memory: 18.2 MB (45.00%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -4,19 +4,17 @@ class Solution:
         for i in grid:
             for j in i:
                 arr.append(j)
+
         res = []
         for i in arr:
             if arr.count(i) == 2:
                 res.append(i)
                 break
-        
+
         seen = set(arr)
-        for i in range(1 , len(arr)+1):
+        for i in range(1 , len(arr) + 1):
             if i not in seen:
                 res.append(i)
                 break
         
         return res
-
-
-            
