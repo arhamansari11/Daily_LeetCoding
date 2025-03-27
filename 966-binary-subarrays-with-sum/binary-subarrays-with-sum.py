d975bf8f6f867b2de332f982cd79604b05e98c7c
Muhammad Arham <arhamansari454647@gmail.com>
2025-03-27 14:39:28 +0500

    Time: 30 ms (66.66%) | Memory: 18.8 MB (62.31%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -3,15 +3,16 @@ class Solution:
         def helper(x):
             if x < 0:
                 return 0
-            res = 0
+            
             l = 0
-            cur = 0
+            current = 0
+            result = 0
             for r in range(len(nums)):
-                cur += nums[r]
-                while cur > x:
-                    cur -= nums[l]
+                current += nums[r]
+                while current > x:
+                    current -= nums[l]
                     l += 1
-                res += (r - l + 1)
-            return res
+                result += (r - l + 1)
+            return result
 
         return helper(goal) - helper(goal - 1)
