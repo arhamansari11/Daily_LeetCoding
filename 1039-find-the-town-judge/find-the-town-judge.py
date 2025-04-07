a27f9cb953d61f11f155b1334f5b9f7dfb0e085e
Muhammad Arham <arhamansari454647@gmail.com>
2025-04-07 15:22:09 +0500

    Time: 11 ms (77.19%) | Memory: 20.9 MB (80.89%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,13 +1,13 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        trust_scores = [0] * (n + 1)
-
+        arr = [0] * (n + 1)
+        
         for a , b in trust:
-            trust_scores[a] -= 1
-            trust_scores[b] += 1
+            arr[a] -= 1
+            arr[b] += 1
 
-        for person in range(1, n + 1):
-            if trust_scores[person] == n - 1:
-                return person 
+        for i in range(1 , n + 1):
+            if arr[i] == n - 1:
+                return i
 
-        return -1  # No judge fo
+        return -1 
