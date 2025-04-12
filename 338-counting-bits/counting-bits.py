1db24ad162eeb63ccce2c2345bafef8dec12cdea
Muhammad Arham <arhamansari454647@gmail.com>
2025-04-12 15:23:38 +0500

    Time: 15 ms (32.67%) | Memory: 18.7 MB (22.81%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,7 +1,7 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         arr = []
-        for i in range(n+1):
+        for i in range(n + 1):
             bit = bin(i)
             split = bit[2:]
             counting = split.count('1')
